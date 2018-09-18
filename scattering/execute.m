4960cda4f8ee340a50bd9be37fc6d4d1fd1f40fe
cwhypt <cwhypt@connect.hku.hk>
2018-09-18 13:49:41 +0800

    uudd1- angle

--- scattering/execute.m
+++ scattering/execute.m
@@ -11,7 +11,7 @@ for k=0.5:0.5:5
         count=count+1;
 
     end
-    flag0.plotsub(h,2,5,3,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','¦È_2 (pi)','Ylabel','|t^L_{u-d}|^2 and |t^L_{d-u}|^2','title',sprintf('Incident E=%0.1feV',k),'Y1legend',sprintf('U->D'),'Y2legend',sprintf('D->U'),'save','true','clear','false');
+    flag0.plotsub(h,2,5,3,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','¦È_2 (pi)','Ylabel','|t^L_{u-u}|^2 and |t^L_{d-d}|^2','title',sprintf('Incident E=%0.1feV',k),'Y1legend',sprintf('U->D'),'Y2legend',sprintf('D->U'),'save','true','clear','false');
     
 end
 
