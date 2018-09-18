b01210e5b1c859f3dc6355aa1ece48d71653f4d9
cwhypt <cwhypt@connect.hku.hk>
2018-09-18 12:29:03 +0800

    Fix y-axis

--- scattering/execute.m
+++ scattering/execute.m
@@ -11,7 +11,7 @@ for k=0.5:0.5:5
         count=count+1;
 
     end
-    flag0.plotsub(h,2,5,3,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','¦È_2 (0-pi)','Ylabel','Probability','title',sprintf('P(E=%0.1feV)',k),'Y1legend',sprintf('U->D'),'Y2legend',sprintf('D->U'),'save','true','clear','false');
+    flag0.plotsub(h,2,5,3,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','¦È_2 (0-pi)','Ylabel','|t^L_{u-d}|^2 and |t^L_{d-u}|^2','title',sprintf('P(E=%0.1feV)',k),'Y1legend',sprintf('U->D'),'Y2legend',sprintf('D->U'),'save','true','clear','false');
     
 end
 
