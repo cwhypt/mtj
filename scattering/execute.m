00666a6592de0fd2ebf72a5f52f9ae75df1b20e3
cwhypt <cwhypt@connect.hku.hk>
2018-09-18 13:59:13 +0800

    uudd -2 energy

--- scattering/execute.m
+++ scattering/execute.m
@@ -1,17 +1,17 @@
-T_x=linspace(0,1,21)';
-T_sc=zeros(21,2);
+T_x=linspace(0,5,51)';
+T_sc=zeros(51,2);
 count=1;
 global flag0
 flag0=flag;
 h=figure;
-for k=0.5:0.5:5
+for j=0:pi/9:pi
     count=1;
-    for j=0:pi/20:pi
+    for k=0:0.1:5
         [T_sc(count,1),T_sc(count,2)]=T(k,j);
         count=count+1;
 
     end
-    flag0.plotsub(h,2,5,3,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','¦È_2 (pi)','Ylabel','|t^L_{u-u}|^2 and |t^L_{d-d}|^2','title',sprintf('Incident E=%0.1feV',k),'Y1legend',sprintf('U->U'),'Y2legend',sprintf('D->D'),'save','true','clear','false');
+    flag0.plotsub(h,2,5,3,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','E_{inc} (eV)','Ylabel','|t^L_{u-u}|^2 and |t^L_{d-d}|^2','title',sprintf('Angle ¦È_2=%0.1fpi',j/pi),'Y1legend',sprintf('U->U'),'Y2legend',sprintf('D->D'),'save','true','clear','false');
     
 end
 
