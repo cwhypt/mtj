56b47ee70a9daed878f8d69becd29f8c46b25b10
cwhypt <cwhypt@connect.hku.hk>
2018-09-18 15:20:23 +0800

    One-pic Einc

--- scattering/execute.m
+++ scattering/execute.m
@@ -4,6 +4,7 @@ count=1;
 global flag0
 flag0=flag;
 h=figure;
+h.Position=[50 50 1200 700];
 for j=0:pi/9:pi
     count=1;
     for k=0:0.1:5
@@ -11,7 +12,9 @@ for j=0:pi/9:pi
         count=count+1;
 
     end
-    flag0.plotsub(h,2,5,3,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','E_{inc} (eV)','Ylabel','|t^L_{u-u}|^2 and |t^L_{d-d}|^2','title',sprintf('Angle ¦È_2=%0.1fpi',j/pi),'Y1legend',sprintf('U->U'),'Y2legend',sprintf('D->D'),'save','true','clear','false');
+    flag0.plotsev(h,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','E_{inc} (eV)','Ylabel','|t^L_{u-u}|^2 and |t^L_{d-d}|^2','title',sprintf('Prob vs. E_{inc}'),'Y1legend',sprintf('UU-¦È_2=%0.1f',j/pi),'Y2legend',sprintf('DD-¦È_2=%0.1f',j/pi),'save','true','clear','false');
     
 end
 
+figure(h)
+legend(flag0.plotsev_leg)
