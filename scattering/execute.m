ebead75e48d48360fcd5d97c843fe836e4764caa
cwhypt <cwhypt@connect.hku.hk>
2018-09-19 11:32:19 +0800

    One-pic theta

--- scattering/execute.m
+++ scattering/execute.m
@@ -1,17 +1,23 @@
-T_x=linspace(0,5,51)';
+T_x=linspace(0,1,51)';
 T_sc=zeros(51,2);
 count=1;
 global flag0
 flag0=flag;
 h=figure;
-for j=0:pi/9:pi
+h.Position=[50 50 1200 700];
+for k=0.5:0.5:5  
     count=1;
-    for k=0:0.1:5
+    for j=0:pi/50:pi
         [T_sc(count,1),T_sc(count,2)]=T(k,j);
         count=count+1;
-
+        
     end
-    flag0.plotsub(h,2,5,3,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','E_{inc} (eV)','Ylabel','|t^L_{u-u}|^2 and |t^L_{d-d}|^2','title',sprintf('Angle ¦È_2=%0.1fpi',j/pi),'Y1legend',sprintf('U->U'),'Y2legend',sprintf('D->D'),'save','true','clear','false');
+    T_sc_order=10.^floor( log10(max(T_sc))); %¼ÆËãÖ¸Êý  %max(T_sc);
+    T_sc_order_matrix=repmat(T_sc_order,51,1);
+    T_sc=T_sc./T_sc_order_matrix;
+    flag0.plotsev(h,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','¦È_2 (pi)','Ylabel','|t^L_{u-u}|^2 and |t^L_{d-d}|^2','title',sprintf('Prob vs. ¦È_2'),'Y1legend',sprintf('Uu-E_{inc}=%0.1feV-%0.0e',k,T_sc_order(1)),'Y2legend',sprintf('Dd-E_{inc}=%0.1feV-%0.0e',k,T_sc_order(2)),'save','true','clear','false');
     
 end
 
+figure(h)
+legend(flag0.plotsev_leg)
