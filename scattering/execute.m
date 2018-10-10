2fa2b6b9b1cc3e4b83a368dbc33e75270d0a3519
cwhypt <cwhypt@connect.hku.hk>
2018-10-10 17:29:36 +0800

    For plotting

--- scattering/execute.m
+++ scattering/execute.m
@@ -8,5 +8,5 @@ for j=0:0.02:1
         count=count+1;
 end
 
-flag0.plot2(3,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','E(0-1eV)','Ylabel','Probability','title','Tunneling Probability','save','true');
+flag0.plot2(3,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','E(0-1eV)','Ylabel','Probability','title','Tunneling Probability','Y1legend','|t_{uu}|^2+t_{du}|^2','Y2legend','|t_{ud}|^2+t_{dd}|^2', 'save','true');
 
