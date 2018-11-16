06729ea54ac28a9a976c3b3dbbf90a5183f5b0b2
Weihao Cao <github@weihaocao.com>
2018-11-16 12:47:01 +0800

    Update -1116

--- scattering/execute.m
+++ scattering/execute.m
@@ -1,21 +1,21 @@
 T_x=linspace(0,1,51)';
-T_sc=zeros(51,2);
+T_sc=zeros(10,2);
 count=1;
 global flag0
 flag0=flag;
 h=figure;
-h.Position=[50 50 1200 700];
-for k=0.5:0.5:5  
+h.Position=[50 0 1200 700];  %50 50 
+for k=0.025:0.025:0.25  
     count=1;
     for j=0:pi/50:pi
         [T_sc(count,1),T_sc(count,2)]=T(k,j);
         count=count+1;
         
     end
-    T_sc_order=10.^floor( log10(max(T_sc))); %计算指数  %max(T_sc);
-    T_sc_order_matrix=repmat(T_sc_order,51,1);
-    T_sc=T_sc./T_sc_order_matrix;
-    flag0.plotsev(h,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','θ_2 (pi)','Ylabel','|t^L_{u-u}|^2 and |t^L_{d-d}|^2','title',sprintf('Prob vs. θ_2'),'Y1legend',sprintf('Uu-E_{inc}=%0.1feV-%0.0e',k,T_sc_order(1)),'Y2legend',sprintf('Dd-E_{inc}=%0.1feV-%0.0e',k,T_sc_order(2)),'save','true','clear','false');
+    %T_sc_order=10.^floor( log10(max(T_sc))); %计算指数  %max(T_sc);
+    %T_sc_order_matrix=repmat(T_sc_order,51,1);
+    %T_sc=T_sc./T_sc_order_matrix;
+    flag0.plotsub(h,2,5,1,T_x,T_sc(:,1),T_sc(:,2), 'Xlabel','θ_2 (pi)','Ylabel','|t^L_{u-d}|^2 and |t^L_{d-u}|^2','title',sprintf('Prob vs. θ_2'),'Y1legend',sprintf('UD-E_{inc}=%0.3feV',k),'Y2legend',sprintf('DU-E_{inc}=%0.3feV',k),'save','true','clear','false');
     
 end
 
