3fed371c9ea0ba056bd83dc6d190426f9108acc6
Weihao Cao <github@weihaocao.com>
2018-11-27 17:56:23 +0800

    checkpoint-compare theoretical value with calculated

--- scattering/N.m
+++ scattering/N.m
@@ -4,16 +4,16 @@ Psil2=Psi(276,:);   Psir2=Psi(411,:);
 
 Qleft1=imag([conj(Psil1(1))*Psil1(4)+conj(Psil1(2))*Psil1(3); ...
             -1i*conj(Psil1(1))*Psil1(4)+1i*conj(Psil1(2))*Psil1(3); ...  %hbar^2/2m
-              conj(Psil1(1))*Psil1(3)+conj(Psil1(2))*Psil1(4)]);
+              conj(Psil1(1))*Psil1(3)-conj(Psil1(2))*Psil1(4)]);
 Qright1=imag([conj(Psir1(1))*Psir1(4)+conj(Psir1(2))*Psir1(3); ...
             -1i*conj(Psir1(1))*Psir1(4)+1i*conj(Psir1(2))*Psir1(3); ...  %hbar^2/2m
-              conj(Psir1(1))*Psir1(3)+conj(Psir1(2))*Psir1(4)]);
+              conj(Psir1(1))*Psir1(3)-conj(Psir1(2))*Psir1(4)]);
 Qleft2=imag([conj(Psil2(1))*Psil2(4)+conj(Psil2(2))*Psil2(3); ...
             -1i*conj(Psil2(1))*Psil2(4)+1i*conj(Psil2(2))*Psil2(3); ...  %hbar^2/2m
-              conj(Psil2(1))*Psil2(3)+conj(Psil2(2))*Psil2(4)]);
+              conj(Psil2(1))*Psil2(3)-conj(Psil2(2))*Psil2(4)]);
 Qright2=imag([conj(Psir2(1))*Psir2(4)+conj(Psir2(2))*Psir2(3); ...
             -1i*conj(Psir2(1))*Psir2(4)+1i*conj(Psir2(2))*Psir2(3); ...  %hbar^2/2m
-              conj(Psir2(1))*Psir2(3)+conj(Psir2(2))*Psir2(4)]);
+              conj(Psir2(1))*Psir2(3)-conj(Psir2(2))*Psir2(4)]);
 n11=Qleft1(1)-Qright1(1);n12=Qleft1(2)-Qright1(2);n13=Qleft1(3)-Qright1(3);
 n21=Qleft2(1)-Qright2(1);n22=Qleft2(2)-Qright2(2);n23=Qleft2(3)-Qright2(3);
 
