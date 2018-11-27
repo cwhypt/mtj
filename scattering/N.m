c5d61b16327517ab366fe5bcc5c5258c77a16924
Weihao Cao <github@weihaocao.com>
2018-11-27 15:07:13 +0800

    spin current test in y-axis, mixed incident states

--- scattering/N.m
+++ scattering/N.m
@@ -1,13 +1,13 @@
-function [n11,n12,n13,n21,n22,n23,cl11,cl12,cl13,cr11,cr12,cr13]=N(M,theta1,kx,Psi)
-Psil1=Psi(69,:);   Psir1=Psi(207,:);
+function [n11,n12,n13,n21,n22,n23,cl11,cl12,cl13,cr11,cr12,cr13,cl21,cl22,cl23,cr21,cr22,cr23]=N(M,theta1,kx,Psi)
+Psil1=Psi(69,:);   Psir2=Psi(207,:);
 Psil2=Psi(276,:);   Psir2=Psi(411,:);
 
 Qleft1=imag([conj(Psil1(1))*Psil1(4)+conj(Psil1(2))*Psil1(3); ...
             -1i*conj(Psil1(1))*Psil1(4)+1i*conj(Psil1(2))*Psil1(3); ...  %hbar^2/2m
               conj(Psil1(1))*Psil1(3)+conj(Psil1(2))*Psil1(4)]);
-Qright1=imag([conj(Psir1(1))*Psir1(4)+conj(Psir1(2))*Psir1(3); ...
-            -1i*conj(Psir1(1))*Psir1(4)+1i*conj(Psir1(2))*Psir1(3); ...  %hbar^2/2m
-              conj(Psir1(1))*Psir1(3)+conj(Psir1(2))*Psir1(4)]);
+Qright1=imag([conj(Psir2(1))*Psir2(4)+conj(Psir2(2))*Psir2(3); ...
+            -1i*conj(Psir2(1))*Psir2(4)+1i*conj(Psir2(2))*Psir2(3); ...  %hbar^2/2m
+              conj(Psir2(1))*Psir2(3)+conj(Psir2(2))*Psir2(4)]);
 Qleft2=imag([conj(Psil2(1))*Psil2(4)+conj(Psil2(2))*Psil2(3); ...
             -1i*conj(Psil2(1))*Psil2(4)+1i*conj(Psil2(2))*Psil2(3); ...  %hbar^2/2m
               conj(Psil2(1))*Psil2(3)+conj(Psil2(2))*Psil2(4)]);
@@ -19,12 +19,19 @@ n21=Qleft2(1)-Qright2(1);n22=Qleft2(2)-Qright2(2);n23=Qleft2(3)-Qright2(3);
 
 %% For debug use
 sigma=[[0 1;1 0],[0,-1i;1i,0],[1 0;0,-1]];
-cl11=[conj(Psil1(1)),conj(Psil1(2))]*sigma(1)*[Psil1(1);Psil1(2)];
-cr11=[conj(Psir1(1)),conj(Psir1(2))]*sigma(1)*[Psir1(1);Psir1(2)];    
-cl12=[conj(Psil1(1)),conj(Psil1(2))]*sigma(2)*[Psil1(1);Psil1(2)];
-cr12=[conj(Psir1(1)),conj(Psir1(2))]*sigma(2)*[Psir1(1);Psir1(2)];    
-cl13=[conj(Psil1(1)),conj(Psil1(2))]*sigma(3)*[Psil1(1);Psil1(2)];
-cr13=[conj(Psir1(1)),conj(Psir1(2))]*sigma(3)*[Psir1(1);Psir1(2)];   
+cl11=Qleft1(1);
+cr11=Qright1(1); 
+cl12=Qleft1(2);
+cr12=Qright1(2);   
+cl13=Qleft1(3);
+cr13=Qright1(3); 
+
+cl21=Qleft2(1);
+cr21=Qright2(1);   
+cl22=Qleft2(2);
+cr22=Qright2(2);    
+cl23=Qleft2(3);
+cr23=Qright2(3);  
 end
 %{
 tuu=M(3,1);tdu=M(3,2);tud=M(4,1);tdd=M(4,2);
--- scattering/test.m
+++ scattering/test.m
@@ -5,22 +5,34 @@ count=1;
 M=zeros(4);
 global flag0
 flag0=flag;
+global flag1
+flag1=flag;
 h=figure(1);
 h.Position=[50 0 1000 600];
+h1=figure(2);
+h1.Position=[50 0 1000 600];
 C_sc1=zeros(51,6);
+C_sc2=zeros(51,6);
  k=0.05  %Energy
     count=1;
     for j=0:pi/50:pi  %Angle
         [T_sc(count,1),T_sc(count,2),M,kx,Psi]=T(k,j);
-         [Q_sc(count,1),Q_sc(count,2),Q_sc(count,3),Q_sc(count,4),Q_sc(count,5),Q_sc(count,6),C_sc1(count,1),C_sc1(count,2),C_sc1(count,3),C_sc1(count,4),C_sc1(count,5),C_sc1(count,6)]=N(M,j,kx,Psi);
+         [Q_sc(count,1),Q_sc(count,2),Q_sc(count,3),Q_sc(count,4),Q_sc(count,5),Q_sc(count,6),C_sc1(count,1),C_sc1(count,2),C_sc1(count,3),C_sc1(count,4),C_sc1(count,5),C_sc1(count,6),C_sc2(count,1),C_sc2(count,2),C_sc2(count,3),C_sc2(count,4),C_sc2(count,5),C_sc2(count,6)]=N(M,j,kx,Psi);
         count=count+1;
         
     end
     
     
-flag0.plotsub3(h,2,2,T_x, Q_sc(:,1), Q_sc(:,2),Q_sc(:,3), 'Xlabel','theta_1 (\pi)','Ylabel','N_{st} for layer 1','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','true','clear','false');
-flag0.plotsub3(h,2,2, T_x, Q_sc(:,4), Q_sc(:,5),Q_sc(:,6), 'Xlabel','theta_1 (\pi)','Ylabel','N_{st} for layer 2','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','true','clear','false');
-flag0.plotsub3(h,2,2,T_x, C_sc1(:,1), C_sc1(:,2),C_sc1(:,3), 'Xlabel','theta_1 (\pi)','Ylabel','Spin in layer1 left','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','true','clear','false');
-flag0.plotsub3(h,2,2, T_x, C_sc1(:,4), C_sc1(:,5),C_sc1(:,6), 'Xlabel','theta_1 (\pi)','Ylabel','Spin in layer1 right','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','true','clear','false');
+flag0.plotsub3(h,2,2,T_x, Q_sc(:,1), Q_sc(:,2),Q_sc(:,3), 'Xlabel','theta_1 (\pi)','Ylabel','N_{st} for layer 1','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','false','clear','false');
+flag0.plotsub3(h,2,2, T_x, Q_sc(:,4), Q_sc(:,5),Q_sc(:,6), 'Xlabel','theta_1 (\pi)','Ylabel','N_{st} for layer 2','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','false','clear','false');
+flag0.plotsub3(h,2,2,T_x, C_sc1(:,1), C_sc1(:,2),C_sc1(:,3), 'Xlabel','theta_1 (\pi)','Ylabel','Spin current in layer1 left','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','false','clear','false');
+flag0.plotsub3(h,2,2, T_x, C_sc1(:,4), C_sc1(:,5),C_sc1(:,6), 'Xlabel','theta_1 (\pi)','Ylabel','Spin current in layer1 right','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','false','clear','false');
 hold off
+
+flag1.plotsub3(h1,2,2,T_x, Q_sc(:,1), Q_sc(:,2),Q_sc(:,3), 'Xlabel','theta_1 (\pi)','Ylabel','N_{st} for layer 1','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','false','clear','false');
+flag1.plotsub3(h1,2,2, T_x, Q_sc(:,4), Q_sc(:,5),Q_sc(:,6), 'Xlabel','theta_1 (\pi)','Ylabel','N_{st} for layer 2','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','false','clear','false');
+flag1.plotsub3(h1,2,2,T_x, C_sc2(:,1), C_sc2(:,2),C_sc2(:,3), 'Xlabel','theta_1 (\pi)','Ylabel','Spin current in layer2 left','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','false','clear','false');
+flag1.plotsub3(h1,2,2, T_x, C_sc2(:,4), C_sc2(:,5),C_sc2(:,6), 'Xlabel','theta_1 (\pi)','Ylabel','Spin current in layer2 right','title',sprintf('Incident E=%0.2feV',k),'Y1legend',sprintf('x'),'Y2legend',sprintf('y'),'Y3legend',sprintf('z'),'save','false','clear','false');
+hold off
+
 
