76f38e91fefdd06f663cd9e68a440fe7d4358a90
Weihao Cao <github@weihaocao.com>
2018-12-06 21:14:56 +0800

    FIx issues with respect to 1layer

--- scattering/T1layer.m
+++ scattering/T1layer.m
@@ -1,5 +1,4 @@
 function [puu,pdd,pdu,pud,ruu,rdd,rdu,rud,M,kx,Psi]=T1layer(a3_E_1,theta)
-%% 定义基本量
 % Assume the width is 3 nm
 global flag1
 flag1=flag;
@@ -15,10 +14,13 @@ a0_Vc=zeros(3000,1);
 a0_Vc(1:276,1)=4;
 a0_Vc(277:828,1)=-0.12;
 a0_Vc(829:1104,1)=4;
+a0_Vc(1105:1656,1)=-0.12;
+a0_Vc(1657:1932,1)=4;
+%a0_Vc=a0_Vc+linspace(1,3000,3000)'./1932.*a5_alpha; %单位？
 %plot(a0_Vc)
 
 % 定义每一步长度  Defines the number of steps(in 1/2 step)
-z_length=1104;
+z_length=1932;
 z_spacing=2;    %69
 global a1_M
 a1_M=floor(z_length/z_spacing);
@@ -46,7 +48,7 @@ a4_theta2=0;
 %% 定义B matrix
 % 定义 B
 b0_Ul=0;
-b0_Ur=a5_alpha.*0.3.*e;
+b0_Ur=0;%a5_alpha.*0.3.*e;
 
 
 b1_kl=sqrt(2*me*(a3_E-b0_Ul)/hbar^2);
@@ -75,7 +77,7 @@ clear -regexp ^b1_Bl_
 clear -regexp ^b1_Br_
 %% 定义D(x)
 D0_21_first=me*0.5*e/(hbar)^2*(sin(a4_theta1).*sigma_x +cos(a4_theta1).*sigma_z);  %   0.5
-%D0_21_second=me*0.5*e/(hbar)^2*(sin(a4_theta2).*sigma_x +cos(a4_theta2).*sigma_z);
+D0_21_second=me*0.5*e/(hbar)^2*(sin(a4_theta2).*sigma_x +cos(a4_theta2).*sigma_z);
 
 D0=[zeros(2) eye(2);zeros(2) zeros(2)];
 D1_D=zeros(a1_M*4+4,4);
@@ -86,7 +88,8 @@ D0_it=D0;
 
 if (j-1)*z_spacing>=277 && (j-1)*z_spacing<=828
     D0_it(3:4,1:2)=D0_21_first;
-
+elseif (j-1)*z_spacing>=1105 && (j-1)*z_spacing<=1656
+    D0_it(3:4,1:2)=D0_21_second;
 end
 
 D0_tmp=-2.*me./hbar^2.*(a3_E-a2_Vd(j,1)).*eye(2);
@@ -129,7 +132,6 @@ Tau=Tm*b1_Bl;
 M_1=[Tau(1:2,3:4) -b1_Br(1:2,3:4); -Tau(3:4,3:4) b1_Br(3:4,3:4)];
 M_2=[-Tau(1:2,1:2) b1_Br(1:2,1:2); Tau(3:4,1:2) -b1_Br(3:4,1:2)];
 M=M_1\M_2;
-
 
 M_real=conj(M).*M; % important
 
