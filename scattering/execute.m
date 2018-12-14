8c1938240f0d2df0b26b01d0abb359fcabcc99fb
cwhypt <cwhypt@gmail.com>
2018-12-14 16:12:12 +0800

    #121703 Plot wavefunctions of sym/asy

--- scattering/execute.m
+++ scattering/execute.m
@@ -27,12 +27,24 @@ flag0.plot2(h,T_x, T_sc(:,1), T_sc(:,2),'Xlabel','Energy (eV)','Ylabel','Tunneli
 %flag0.plot2(h,T_x, T_sc(:,1), T_sc(:,2),'Xlabel','Energy (eV)','Ylabel','Tunneling prob','title',sprintf('Layer 2 angle =%0.2f pi',j/pi),'Y1legend',sprintf('up'),'Y2legend',sprintf('down'),'save','true','clear','true');
 
 end
-
+flag0=flag;
+h1=figure(2);
+h1.Position=[50 0 1000 600];
 sz=size(T_sumE);
 flag0.plot2(h1,linspace(0,1,sz(1)), T_sumE(:,1), T_sumE(:,2),'Xlabel',sprintf('Layer angle'),'Ylabel','State energy','title',sprintf('Layer 1 angle =%0.2f pi',j/pi),'Y1legend',sprintf('up-sym'),'Y2legend',sprintf('up-asy'),'save','true','clear','false');
 hold on
 flag0.plot2(h1,linspace(0,1,sz(1)), T_sumE(:,3), T_sumE(:,4),'Xlabel',sprintf('Layer angle'),'Ylabel','State energy','title',sprintf('Layer 1 angle =%0.2f pi',j/pi),'Y1legend',sprintf('down-sym'),'Y2legend',sprintf('down-asy'),'save','true','clear','false');
 hold on
+
+%% Plot wavefunctions of the special energies
+flag0=flag;
+h2=figure(3);
+h2.Position=[50 0 1000 600];
+E=0.328;j=0;
+[T_sc(count,1),T_sc(count,2),M,kx,Psi]=T(E,j); 
+%Psi=conj(Psi).*Psi;
+sz_Psi=size(Psi);
+flag0.plot2(h2,linspace(0,1932,sz_Psi(1)), Psi(:,1),Psi(:,2),'Xlabel','Location (nm)','Ylabel','Prob','title',sprintf('Eigenfunction at E=%0.2f',E),'Y1legend',sprintf('up'),'Y2legend',sprintf('down'),'save','true','clear','false');
 
 
 
