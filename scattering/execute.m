d3bb46fb1aae32c069c99111664584423ca58792
cwhypt <cwhypt@gmail.com>
2018-12-14 15:54:13 +0800

    121702 State Energy

--- scattering/execute.m
+++ scattering/execute.m
@@ -21,9 +21,18 @@ for j=0:pi/50:pi  %Energy
      [T_sort,T_index]=sort(T_sc,1,'descend');T_index=sort(T_index(1:2,:),1,'ascend');
      T_sort=[T_sc(T_index(1,1),1),T_sc(T_index(2,1),1),T_sc(T_index(1,2),2),T_sc(T_index(2,2),2)]; %描述上1上2，下1下2幅度
      T_ener=[T_index(1,1),T_index(2,1),T_index(1,2),T_index(2,2)]*0.002;
-     T_sumE=[T_sumE, T_ener];T_sumP=[T_sumP,T_sort];
+     T_sumE=[T_sumE; T_ener];T_sumP=[T_sumP;T_sort];
     
 flag0.plot2(h,T_x, T_sc(:,1), T_sc(:,2),'Xlabel','Energy (eV)','Ylabel','Tunneling prob','title',sprintf('Layer 2 angle =%0.2f pi',j/pi),'Y1legend',sprintf('up'),'Y2legend',sprintf('down'),'save','true','clear','true');
 %flag0.plot2(h,T_x, T_sc(:,1), T_sc(:,2),'Xlabel','Energy (eV)','Ylabel','Tunneling prob','title',sprintf('Layer 2 angle =%0.2f pi',j/pi),'Y1legend',sprintf('up'),'Y2legend',sprintf('down'),'save','true','clear','true');
 
 end
+
+sz=size(T_sumE);
+flag0.plot2(h1,linspace(0,1,sz(1)), T_sumE(:,1), T_sumE(:,2),'Xlabel',sprintf('Layer angle'),'Ylabel','State energy','title',sprintf('Layer 1 angle =%0.2f pi',j/pi),'Y1legend',sprintf('up-sym'),'Y2legend',sprintf('up-asy'),'save','true','clear','false');
+hold on
+flag0.plot2(h1,linspace(0,1,sz(1)), T_sumE(:,3), T_sumE(:,4),'Xlabel',sprintf('Layer angle'),'Ylabel','State energy','title',sprintf('Layer 1 angle =%0.2f pi',j/pi),'Y1legend',sprintf('down-sym'),'Y2legend',sprintf('down-asy'),'save','true','clear','false');
+hold on
+
+
+
